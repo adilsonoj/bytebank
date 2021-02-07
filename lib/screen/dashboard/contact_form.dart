9dd12dc35870d1e9695a628dce236ae7b828e233
Adilson Oliveira <adilsonoj@yahoo.com.br>
2021-02-07 20:15:06 -0300

    adiciona SingleChildScrollView

--- lib/screen/dashboard/contact_form.dart
+++ lib/screen/dashboard/contact_form.dart
@@ -17,33 +17,35 @@ class _ContactFormState extends State<ContactForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("New Contact")),
-      body: Column(
-        children: [
-          TextEdit(
-            label: "Name",
-            value: _name,
-            type: "Text",
-          ),
-          TextEdit(
-            value: _accountNumber,
-            label: "Account Number",
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              width: double.maxFinite,
-              child: RaisedButton(
-                onPressed: () {
-                  final String name = _name.text;
-                  final int accountNumber = int.tryParse(_accountNumber.text);
-                  final Contact newContact = Contact(0, name, accountNumber);
-                  _dao.save(newContact).then((id) => Navigator.pop(context));
-                },
-                child: Text('Create'),
-              ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextEdit(
+              label: "Name",
+              value: _name,
+              type: "Text",
             ),
-          )
-        ],
+            TextEdit(
+              value: _accountNumber,
+              label: "Account Number",
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  onPressed: () {
+                    final String name = _name.text;
+                    final int accountNumber = int.tryParse(_accountNumber.text);
+                    final Contact newContact = Contact(0, name, accountNumber);
+                    _dao.save(newContact).then((id) => Navigator.pop(context));
+                  },
+                  child: Text('Create'),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
